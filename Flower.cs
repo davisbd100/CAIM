26c9615d96a8ed0cd853e771532743f9a131615b
David <davisbd100@gmail.com>
2020-05-27 14:00:09 -0500

    Agregada clase Flower y MainClass

---
+++ Flower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIM
+{
+    public class Flower
+    {
+        public double SepalLength { get; set; }
+        public double SepalWidth { get; set; }
+        public double PetalLength { get; set; }
+        public double PetalWidth { get; set; }
+        public String ClassName { get; set; }
+
+        public Flower(double sepalLength, double sepalWidth, double petalLength, double petalWidth, String className)
+        {
+            this.SepalLength = sepalLength;
+            this.SepalWidth = sepalWidth;
+            this.PetalLength = petalLength;
+            this.PetalWidth = petalWidth;
+            this.ClassName = className;
+        }
+    }
+}
